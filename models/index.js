b2958a4c9250178257386a04a86dea43669b622b
Hayden-Haun <haydenhaun@gmail.com>
2021-09-25 16:09:15 -0600

    started static html design

--- models/index.js
+++ models/index.js
@@ -1,3 +1,16 @@
-const User = require('./User');
+const User = require("./User");
+
+//create relationships between data
+
+//1. HOMEPAGE
+//  --- include existing blog posts if any have been posted
+//  --- include NAVBAR links to the homepage and dashboard, option to log in
+//** */
+//  ---  when user clicks any other nav button, they are taken to the login/sign-up page if not logged in
+//  --- when user chooses to sign-up, they are prompted to create username.password
+
+//2. WHEN SIGNED IN
+//  ---see nav links to the homepage, dashboard, and the option to log-out
+//
 
 module.exports = { User };
