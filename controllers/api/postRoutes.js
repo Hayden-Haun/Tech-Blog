9811664c963b9dc0e837b0c9d39d5f725ba0d3f6
Hayden-Haun <haydenhaun@gmail.com>
2021-09-26 02:03:49 -0600

    pGET routes in postRoutes are complete: return all and return specific id

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -4,7 +4,27 @@ const { Post, User, Comment } = require("../../models");
 // --- /api/posts/home
 router.get("/home", async (req, res) => {
   try {
-    const postData = await Post.findAll({ include: [User] });
+    const postData = await Post.findAll({
+      include: [User, Comment],
+    });
+    res.status(200).json(postData);
+  } catch {
+    res.status(500).json(err);
+  }
+});
+
+// --- /api/posts/:post_id
+router.get("/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [User, Comment],
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: "No post with this id!" });
+      return;
+    }
+
     res.status(200).json(postData);
   } catch {
     res.status(500).json(err);
