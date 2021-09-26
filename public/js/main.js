ceb7347d16ea0edad5bc68b9ccd019e34b02be4e
Hayden-Haun <haydenhaun@gmail.com>
2021-09-26 14:27:13 -0600

    added boiler plate code for js/main.js to control routes for navBar icons

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+const renderHomePage = (event) => {};
+
+const renderPostCreatePage = (event) => {};
+
+//Event listeners for navBar icons: Home & CreatePost
+
+document.getElementById("homeBtn").addEventListener("click", renderHomePage);
+
+document
+  .getElementById("createPostBtn")
+  .addEventListener("click", loginFormHandler);
