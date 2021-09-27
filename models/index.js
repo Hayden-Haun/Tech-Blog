0a339d7fe1e1b836e33e7f4ad14a711b03e196b5
Hayden-Haun <haydenhaun@gmail.com>
2021-09-27 16:42:29 -0600

    updated relationships in models/index.js. Comments are now attached to posts instead of users

--- models/index.js
+++ models/index.js
@@ -10,7 +10,7 @@ User.hasMany(Comment, { foreignKey: "comment_author" });
 
 // Posts belong to users and can have many comments.
 Post.belongsTo(User, { foreignKey: "post_author" });
-Post.hasMany(Comment, { foreignKey: "comment_author" });
+Post.hasMany(Comment, { foreignKey: "comment_parent_post" });
 
 // Comments belong to Users & Posts
 Comment.belongsTo(User, { foreignKey: "comment_author" });
