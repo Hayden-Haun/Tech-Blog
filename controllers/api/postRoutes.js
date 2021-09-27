f54422bdbb40ef7fbdc5fecae729c183491a1ef5
Hayden-Haun <haydenhaun@gmail.com>
2021-09-27 15:44:28 -0600

    lots of progress on commentbiew.handlebars

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -45,16 +45,44 @@ router.get("/:id", async (req, res) => {
       include: [User, Comment],
     });
 
-    if (!postData) {
+    // console.log(postData.comments);
+
+    const { comments, user, post_title, post_text, post_author } = postData;
+    // console.log(comments);
+    // console.log(user);
+
+    const commentArray = comments.map((data) => data.get({ plain: true }));
+    // const { posts } = postData
+    // console.log(posts);
+    // console.log(commentArray);
+
+    // console.log(post_title);
+    // console.log(post_author);
+    // console.log(post_text);
+
+    const postName = await User.findOne({ where: { id: post_author } });
+    if (postName === null) {
+      console.log("Not found!");
+    } else {
+      console.log(postName instanceof User); // true
+      console.log(postName.username); // 'My Title'
+    }
+
+    postAuthor = postName.username;
+
+    if (!postName) {
       res.status(404).json({ message: "No post with this id!" });
       return;
     }
 
+    // console.log(commentArray);
     // res.status(200).json(postData);
     res.render("commentview", {
-      Post,
-      User,
-      Comment,
+      post_title,
+      post_text,
+      postAuthor,
+      user,
+      commentArray,
     });
   } catch {
     res.status(500).json(err);
