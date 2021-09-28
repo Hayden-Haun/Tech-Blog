24fb0061253834c033d30aa8776dd91873fe0edf
Hayden-Haun <haydenhaun@gmail.com>
2021-09-27 20:23:26 -0600

    event listeners now return the button id

--- public/js/postPage.js
+++ public/js/postPage.js
@@ -2,27 +2,35 @@
 const renderCommentsPage = (event) => {
   event.preventDefault();
 
-  //pull new post data from input fields
-  const post_id = document.querySelector("#post_title_input").value.trim();
+  alert(`this is working!!! ${event.target.id}`);
 
-  if (post_id) {
-    const response = await fetch("api/posts/new", {
-      method: "POST",
-      body: JSON.stringify({ post_title, post_author, post_text }),
-      headers: { "Content-Type": "application/json" },
-    });
+  // //pull new post data from input fields
+  // const post_id = document.querySelector("#post_title_input").value.trim();
 
-    if (response.ok) {
-      //   alert("reponse ok ");
-      document.location.replace("/");
-    } else {
-      alert("failed");
-    }
-  }
+  // if (post_id) {
+  //   const response = await fetch("api/posts/new", {
+  //     method: "POST",
+  //     body: JSON.stringify({ post_title, post_author, post_text }),
+  //     headers: { "Content-Type": "application/json" },
+  //   });
+
+  //   if (response.ok) {
+  //     //   alert("reponse ok ");
+  //     document.location.replace("/");
+  //   } else {
+  //     alert("failed");
+  //   }
+  // }
 };
 
 //EVENT LISTENER FOR BUTTONS
 
-document
-  .querySelector(".viewCommentsButton")
-  .addEventListener("click", renderCommentsPage);
+const buttons = document.querySelectorAll(".viewCommentsButton");
+
+buttons.forEach(function (currentBtn) {
+  currentBtn.addEventListener("click", renderCommentsPage);
+});
+
+// document
+//   .querySelector(".viewCommentsButton")
+//   .addEventListener("click", renderCommentsPage, false);
