3278e45391ce5ac5c2651a5df2df7fef81b44dd8
Hayden-Haun <haydenhaun@gmail.com>
2021-09-27 20:39:34 -0600

    render comment page with extra key value pair logged in so that logout button renders

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -69,7 +69,7 @@ router.get("/:id", async (req, res) => {
       return { author: value, text: commentArray[index].comment_text };
     });
 
-    console.log(commentData);
+    // console.log(commentData);
 
     const postName = await User.findOne({ where: { id: post_author } });
     const postAuthor = postName.username;
@@ -86,6 +86,7 @@ router.get("/:id", async (req, res) => {
       postAuthor,
       user,
       commentData,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
