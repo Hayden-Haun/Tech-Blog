2d1762a297fcaa750b03167e0672cd62eaaaab00
Hayden-Haun <haydenhaun@gmail.com>
2021-09-27 21:28:49 -0600

    saving progress on new comment functionality

--- public/js/commentPage.js
+++ public/js/commentPage.js
@@ -1,6 +1,27 @@
 const createNewComment = (event) => {
   event.preventDefault();
-  alert(event.target.id);
+  //   alert(event.target.id);
+  const comment_parent_post = event.target.id;
+  const comment_author = 20;
+  const comment_text = document.querySelector("#newComment").value.trim();
+
+  if (comment_text) {
+    const response = await fetch("api/comments/new", {
+      method: "POST",
+      body: JSON.stringify({
+        comment_parent_post,
+        comment_author,
+        comment_text,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace(`/api/posts/${event.target.id}`);
+    } else {
+      alert("FAILED");
+    }
+  }
 };
 
 document
